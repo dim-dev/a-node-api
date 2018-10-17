494182a3ba40e6c5a8412b73cb4fa02a071cab4b
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-10-17 14:06:10 +0200

    Single book.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const debug = require('debug');
+const debug = require('debug')('appjs:');
 const mongoose = require('mongoose');
 const Book = require('./models/bookModel');
 
-const db = mongoose.connect('mongodb://localhost/book-api');
+const db = mongoose.connect('mongodb://localhost:27017/book-api', { useNewUrlParser: true });
 
 const app = express();
 const port = process.env.port || 3000;
@@ -13,12 +13,19 @@ const bookRouter = express.Router();
 bookRouter.route('/Books')
   .get((req, res) => {
     const query = {};
-
+    debug(req.query.genre);
     if (req.query.genre) {
       query.genre = req.query.genre;
     }
     Book.find(query, (err, books) => {
       if (err) { res.status(500).send(err); } else { res.json(books); }
+    });
+  });
+
+bookRouter.route('/Books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) { res.status(500).send(err); } else { res.json(book); }
     });
   });
 
--- models/bookModel.js
+++ models/bookModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const mongooseSchema = mongoose.Schema;
+const Schema = mongooseSchema;
 
 const bookModel = new Schema({
-  title: {
-    type: String,
-  },
+  title: { type: String },
   author: { type: String },
   genre: { type: String },
   read: { type: Boolean, default: false },
