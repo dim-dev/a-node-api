506f578a84266161374771276dfb3555fdb7e395
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-10-18 16:54:13 +0200

    tests added.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,8 +3,14 @@ const Book = require('../models/bookModel');
 const bookctrl = {
   post: (req, res) => {
     const book = new Book(req.body);
+
+    if (!req.body.title) {
+      res.status(400);
+      res.send('title is required.');
+    }
     book.save();
-    res.status(201).send(book);
+    res.status(201);
+    res.send(book);
   },
   get: (req, res) => {
     const query = {};
